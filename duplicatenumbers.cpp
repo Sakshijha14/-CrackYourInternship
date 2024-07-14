75feee1d8f185c5b89749772721e883d6603d558
sakshi <saknov14@gmail.com>
2024-07-14 22:19:39 +0530

    DAY1

---
+++ duplicatenumbers.cpp
@@ -0,0 +1,20 @@
+// DAY 1
+//https://leetcode.com/problems/find-the-duplicate-number/
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+        return -1;
+        
+    }
+};
+
