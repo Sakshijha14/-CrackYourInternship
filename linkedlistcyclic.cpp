c929bf9a6bd0f205764f5cef06d9ce87e65be124
sakshi <saknov14@gmail.com>
2024-07-29 19:47:48 +0530

    DAY15

---
+++ linkedlistcyclic.cpp
@@ -0,0 +1,31 @@
+// DAY15
+//https://leetcode.com/problems/linked-list-cycle/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+        ListNode*fast=head;
+        ListNode*slow=head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+
+            if(fast==slow){
+                return true;
+            }
+           
+        }
+     return false;
+    }
+};
