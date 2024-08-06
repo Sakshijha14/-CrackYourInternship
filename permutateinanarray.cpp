f0ace978350f1ccfbe49476c37d287aa6913ec1f
sakshi <saknov14@gmail.com>
2024-08-06 16:21:23 +0530

    DAY23question4

---
+++ permutateinanarray.cpp
@@ -0,0 +1,23 @@
+// DAY23question4
+//https://www.geeksforgeeks.org/problems/permutations-in-array1747/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isPossible(int k, vector<int> &arr1, vector<int> &arr2) {
+        // Your code goes here
+        int n=arr1.size();
+        
+        sort(arr1.begin(),arr1.end());
+        sort(arr2.begin(),arr2.end(),greater<int>());
+        
+        for(int i=0;i<n;i++){
+            if(arr1[i]+arr2[i]<k){
+                return false;
+            }
+        }
+        return true;
+    }
+};
