2f38cbc8f48ab06d0fdd9a8ce727acf305b519e8
sakshi <saknov14@gmail.com>
2024-08-06 22:52:50 +0530

    DAY23question9

---
+++ evaluatereversepolishnotation.cpp
@@ -0,0 +1,49 @@
+// DAY23question9
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+
+        stack<int>st;
+
+        for(string c:tokens){
+            if(c=="+"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first+second);
+            }
+
+            else if(c=="-"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first-second);
+            }
+            else if(c=="*"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first*second);
+            }
+            else if(c=="/"){
+                int second=st.top();
+                st.pop();
+                int first=st.top();
+                st.pop();
+                st.push(first/second);
+            }
+            else{
+                st.push(stoi(c));
+            }
+        }
+    return st.top();   
+    }
+};
