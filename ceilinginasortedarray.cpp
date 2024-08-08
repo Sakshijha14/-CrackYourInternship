3b4058d6f668b4fcce6581de011a22ccbaa2826b
sakshi <saknov14@gmail.com>
2024-08-08 18:34:20 +0530

    DAY25

---
+++ ceilinginasortedarray.cpp
@@ -0,0 +1,24 @@
+// DAY25
+//https://www.geeksforgeeks.org/ceiling-in-a-sorted-array/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int ceilsearch(int arr[],int low,int high,int x){
+
+    int i;
+
+    if(x<=arr[low]){
+        return low;
+    }
+    for(i=low;i<high;i++){
+        if(arr[i]==x){
+            return i;
+        }
+        if(arr[i]<x && arr[i+1]>=x){
+            return i+1;
+        }
+    }
+    return -1;
+}
+
