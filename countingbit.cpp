dace1450d1694908c30cbf6daefa7c0c89ec1530
sakshi <saknov14@gmail.com>
2024-08-12 19:11:22 +0530

    DAY29question4

---
+++ countingbit.cpp
@@ -0,0 +1,28 @@
+// DAY29question4
+//https://leetcode.com/problems/counting-bits/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+int bitcount(int n){
+    int count=0;
+    while(n){
+        if((n&1)==1)
+        count++;
+        n=n>>1;
+    }
+    return count;
+}
+    vector<int> countBits(int n) {
+        int num=0;
+        vector<int>v;
+        for(int i=0;i<=n;i++){
+            int count=bitcount(i);
+            v.push_back(count);
+        }
+        return v;
+    }
+};
